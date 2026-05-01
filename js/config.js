e2e57250433c521f6dc640ff888a18100d13e8de
AstridG1 <97997184+AstridG1@users.noreply.github.com>
2026-05-01 17:50:02 -0600

    update config.js
    new user pool with config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_6MIHN2zVX',
-        userPoolClientId: '6m5bt98eqghi5itad3ch6mdcg1',
+        userPoolId: 'us-east-1_dKYGx99qc',
+        userPoolClientId: '14dvkjo77vd211up7to0vst3o2',
         region: 'us-east-1'
     },
     api: {
