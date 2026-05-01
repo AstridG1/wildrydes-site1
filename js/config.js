0505f840ce70a7ed0bd8aecc0ddbfe191f19d49e
AstridG1 <97997184+AstridG1@users.noreply.github.com>
2026-05-01 17:08:07 -0600

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-1_EnChq17ld',
+        userPoolClientId: '5vqa9a80lgfkkm8isceb5f2jag',
+        region: 'eu-west-1'
+    },
+    api: {
+        invokeUrl: 'https://5eq7r97z7g.execute-api.eu-west-1.amazonaws.com/prod'
+    }
+};
