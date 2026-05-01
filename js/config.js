8e40a9817d5983110f4d3f3561710a4bc4043298
AstridG1 <97997184+AstridG1@users.noreply.github.com>
2026-05-01 17:54:18 -0600

    Update userPoolClientId in config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_dKYGx99qc',
-        userPoolClientId: '14dvkjo77vd211up7to0vst3o2',
+        userPoolClientId: '14dvkjo77vd2llup7to0vst3o2',
         region: 'us-east-1'
     },
     api: {
